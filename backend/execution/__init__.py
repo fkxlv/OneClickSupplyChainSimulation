18e5f032c5b104dd249b2dc3b3261f4db18c7728
ZikPin <ziya.gulu365@gmail.com>
2026-02-08 07:31:46 +0100

    refactoring

--- backend/execution/__init__.py
+++ backend/execution/__init__.py
@@ -2,16 +2,13 @@ import os
 
 from flask import Flask
 from flask_cors import CORS
-from flask_marshmallow import Marshmallow
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     cors = CORS(app)
-    ma = Marshmallow(app)
 
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'server.sqlite')
     )
 
     if test_config is None:
